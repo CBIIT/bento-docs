de595ea87972e5c06652170e772f2e14c296011f
bensonml <bensonml@nih.gov>
2021-01-04 11:09:27 -0500

    regex again

--- source/conf.py
+++ source/conf.py
@@ -113,7 +113,7 @@ smv_remote_whitelist = None
 # A Regular Expression is used to determine if a version of the documentation 
 # has been released or if it’s a development version. To allow more 
 # flexibility, the regex is evaluated over the full refname.
-smv_released_pattern = r'^(heads/release.*]+)/(?!:master).*$'
+smv_released_pattern = r'^(?!master).*$'
 
 # AutoStructify - for advanced markdown to rst transformations
 # Needs to be at the bottom of conf.py
